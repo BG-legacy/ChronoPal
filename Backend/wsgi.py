0b607d94c2a52d3e1eb357a9957b847e031357cf
BG-legacy <bginnjr20@gmail.com>
2025-04-11 01:04:53 -0400

    project completed

--- Backend/wsgi.py
+++ Backend/wsgi.py
@@ -11,4 +11,5 @@ application = app
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("api.main:app", host="0.0.0.0", port=8000, reload=True) 
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("api.main:app", host="0.0.0.0", port=port, reload=True) 
