fcd10eeba54aef751efc64ff5132eaddb144c84b
BG-legacy <bginnjr20@gmail.com>
2025-04-11 03:26:10 -0400

    Set Heroku backend as default API URL

--- frontend/src/services/apiService.ts
+++ frontend/src/services/apiService.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { Pet } from '../types/pet';
 import { User } from '../types/user';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://chronopal-backend-00ae6a240df5.herokuapp.com';
 
 interface InteractionRequest {
   pet_id: string;
